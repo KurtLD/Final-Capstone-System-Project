550757d6d1d3df8f23c13d9af5972dac3011c719
KurtLD <119637287+KurtLD@users.noreply.github.com>
2024-08-07 10:47:45 +0800

    Essential Files

---
+++ C_App_User/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CAppUserConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'C_App_User'
---
+++ E_App_Recommender/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EAppRecommenderConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'E_App_Recommender'
